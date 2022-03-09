4d8bb63169d09727973d5f4ffd4acbcac0b0edaa
Yogesh <you@example.com>
2022-03-09 22:16:45 +0530

    changed let to const

--- jobs-api/middleware/error-handler.js
+++ jobs-api/middleware/error-handler.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
 	console.log('err middleware');
 
-	let customError = {
+	const customError = {
 		// set default
 		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
 		msg: err.message || 'Something went wrong try again later',
