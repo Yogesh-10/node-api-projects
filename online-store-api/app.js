07d1a55f328286c39f8ba4f594986826a8208c43
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-10 21:05:10 +0530

    DB Connected and server setup

--- online-store-api/app.js
+++ online-store-api/app.js
@@ -1,1 +1,24 @@
-console.log('E-Commerce API');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./db/connect');
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, console.log(`Server connected on PORT ${PORT}`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
