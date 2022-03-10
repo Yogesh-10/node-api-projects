7dd46f99621745b0d27e00e53d9b4e0464d86256
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-11 00:02:14 +0530

    error middlewares and morgan setup

--- online-store-api/app.js
+++ online-store-api/app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+require('express-async-errors');
+const morgan = require('morgan');
+
 const connectDB = require('./db/connect');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFound = require('./middleware/not-found');
 
 dotenv.config();
 
 const app = express();
 
+//middlewares
+app.use(morgan('tiny'));
+app.use(express.json()); //to get access to req.body in post and put requests
+
 app.get('/', (req, res) => {
 	res.send('hello');
 });
+
+//error handler middlewares
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
