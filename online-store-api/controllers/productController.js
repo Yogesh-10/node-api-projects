ad24663cf89756efef134d6c243a5283a6367c0d
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-13 12:31:28 +0530

    product routes boiler plate setup

---
+++ online-store-api/controllers/productController.js
@@ -0,0 +1,32 @@
+const createProduct = async () => {
+	res.send('product controller');
+};
+
+const getAllProducts = async () => {
+	res.send('product controller');
+};
+
+const getSingleProduct = async () => {
+	res.send('product controller');
+};
+
+const updateProduct = async () => {
+	res.send('product controller');
+};
+
+const deleteProduct = async () => {
+	res.send('product controller');
+};
+
+const uploadImage = async () => {
+	res.send('product controller');
+};
+
+module.exports = {
+	createProduct,
+	getAllProducts,
+	getSingleProduct,
+	updateProduct,
+	deleteProduct,
+	uploadImage,
+};
---
+++ online-store-api/routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+	authenticeUser,
+	authorizePermissions,
+} = require('../middleware/authentication');
+
+const {
+	createProduct,
+	getAllProducts,
+	getSingleProduct,
+	updateProduct,
+	deleteProduct,
+	uploadImage,
+} = require('../controllers/productController');
+
+router
+	.route('/')
+	.post([authenticeUser, authorizePermissions('admin')], createProduct)
+	.get(getAllProducts);
+
+router
+	.route('/uploadImage')
+	.post([authenticeUser, authorizePermissions('admin')], uploadImage);
+
+router
+	.route('/:id')
+	.get(getSingleProduct)
+	.patch([authenticeUser, authorizePermissions('admin')], updateProduct)
+	.delete([authenticeUser, authorizePermissions('admin')], deleteProduct);
+
+module.exports = router;
