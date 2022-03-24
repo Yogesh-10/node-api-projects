f626660a80167180621499687992411e753fc436
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-25 02:05:29 +0530

    fix in verify

--- auth_advanced/front-end/src/pages/Verify.js
+++ auth_advanced/front-end/src/pages/Verify.js
@@ -3,60 +3,63 @@ import { useLocation, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useGlobalContext } from '../context';
 import axios from 'axios';
+
 function useQuery() {
-  return new URLSearchParams(useLocation().search);
+	return new URLSearchParams(useLocation().search);
 }
 
 const VerifyPage = () => {
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const { isLoading } = useGlobalContext();
-  const query = useQuery();
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const { isLoading } = useGlobalContext();
+	const query = useQuery();
 
-  const verifyToken = async () => {
-    setLoading(true);
-    try {
-      const { data } = await axios.post('/api/v1/auth/verify-email', {
-        verificationToken: query.get('token'),
-        email: query.get('email'),
-      });
-    } catch (error) {
-      // console.log(error.response);
-      setError(true);
-    }
-    setLoading(false);
-  };
+	const verifyToken = async () => {
+		setLoading(true);
+		try {
+			await axios.post('/api/v1/auth/verify-email', {
+				verificationToken: query.get('token'),
+				email: query.get('email'),
+			});
+		} catch (error) {
+			// console.log(error.response);
+			setError(true);
+		}
+		setLoading(false);
+	};
 
-  useEffect(() => {
-    if (!isLoading) {
-      verifyToken();
-    }
-  }, []);
+	useEffect(() => {
+		if (!isLoading) {
+			verifyToken();
+		}
+		// eslint-disable-next-line
+	}, []);
 
-  if (loading) {
-    return (
-      <Wrapper className='page'>
-        <h2>Loading...</h2>
-      </Wrapper>
-    );
-  }
+	if (loading) {
+		return (
+			<Wrapper className='page'>
+				<h2>Loading...</h2>
+			</Wrapper>
+		);
+	}
 
-  if (error) {
-    return (
-      <Wrapper className='page'>
-        <h4>There was an error, please double check your verification link </h4>
-      </Wrapper>
-    );
-  }
+	if (error) {
+		return (
+			<Wrapper className='page'>
+				<h4>There was an error, please double check your verification link </h4>
+			</Wrapper>
+		);
+	}
 
-  return (
-    <Wrapper className='page'>
-      <h2>Account Confirmed</h2>
-      <Link to='/login' className='btn'>
-        Please login
-      </Link>
-    </Wrapper>
-  );
+	return (
+		<Wrapper className='page'>
+			<h2>Account Confirmed</h2>
+			{console.log('acc')}
+			<Link to='/login' className='btn'>
+				Please login
+			</Link>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.section``;
