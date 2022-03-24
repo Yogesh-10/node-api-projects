d8bd45c783a657988884cf997c5a5ea37f01dc14
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-03-24 21:16:49 +0530

    bug fixes

--- auth_advanced/front-end/src/pages/Dashboard.js
+++ auth_advanced/front-end/src/pages/Dashboard.js
@@ -4,31 +4,31 @@ import main from '../assets/main.svg';
 import { Redirect } from 'react-router-dom';
 import { useGlobalContext } from '../context';
 function Dashboard() {
-  const { user } = useGlobalContext();
-  const { name, userId, role } = user;
-  return (
-    <>
-      <Wrapper className='page'>
-        <h2>Hello there, {user.name}</h2>
-        <p>
-          Your ID : <span>{userId}</span>
-        </p>
-        <p>
-          Your Role : <span>{role}</span>
-        </p>
-      </Wrapper>
-    </>
-  );
+	const { user } = useGlobalContext();
+	const { name, userId, role } = user;
+	return (
+		<>
+			<Wrapper className='page'>
+				<h2>Hello there, {name}</h2>
+				<p>
+					Your ID : <span>{userId}</span>
+				</p>
+				<p>
+					Your Role : <span>{role}</span>
+				</p>
+			</Wrapper>
+		</>
+	);
 }
 
 const Wrapper = styled.div`
-  p span {
-    background: var(--primary-500);
-    padding: 0.15rem 0.25rem;
-    color: var(--white);
-    border-radius: var(--borderRadius);
-    letter-spacing: var(--letterSpacing);
-  }
+	p span {
+		background: var(--primary-500);
+		padding: 0.15rem 0.25rem;
+		color: var(--white);
+		border-radius: var(--borderRadius);
+		letter-spacing: var(--letterSpacing);
+	}
 `;
 
 export default Dashboard;
